a2eda78cc1f32802c54ee273691d921f93cb22fb
crackcomm <crackcomm@gmail.com>
2015-12-20 20:51:18 +0100

    Queue cleaner (docs)

--- queue.go
+++ queue.go
@@ -13,11 +13,11 @@ type Job interface {
 type Queue interface {
 	// Get - Gets request from Queue channel.
 	// Returns io.EOF if queue is done/closed.
-	Get() (job Job, err error)
+	Get() (Job, error)
 
 	// Schedule - Schedules a Request.
 	// Returns io.ErrClosedPipe if queue is closed.
-	Schedule(job Job) error
+	Schedule(Job) error
 
 	// Close - Closes the queue.
 	Close() error
