2c4c63428b89b3f088c2da4de86796c7d1ce4aaa
crackcomm <crackcomm@gmail.com>
2015-12-20 23:17:51 +0100

    example: imdb spider

--- examples/imdb/main.go
+++ examples/imdb/main.go
@@ -5,32 +5,30 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/crackcomm/crawl"
-	"golang.org/x/net/context"
+
+	imdb "github.com/crackcomm/crawl/examples/imdb/spider"
 )
 
-var maxReqs = flag.Int("max-reqs", 600, "Max requests per second")
+var spiders = []func(*crawl.Crawl){
+	imdb.Register,
+}
 
 func main() {
-	flag.Parse()
-
 	c := crawl.New(
 		crawl.WithQueue(crawl.NewQueue(1000)),
 		crawl.WithConcurrency(200),
 	)
 
-	spider := &imdbSpider{Crawl: c}
+	for _, spider := range spiders {
+		spider(c)
+	}
 
-	c.Handler(Entity, spider.Entity)
-	c.Handler(List, spider.List)
 	c.Schedule(&crawl.Request{
 		URL:       "http://www.imdb.com/chart/top",
-		Callbacks: crawl.Callbacks(List),
+		Callbacks: crawl.Callbacks(imdb.List),
 	})
 
 	log.Print("Starting crawl")
@@ -46,53 +44,4 @@ func main() {
 	if err := c.Start(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-var (
-	// Entity - Movie entity.
-	Entity = "entity"
-
-	// List - Movies list.
-	List = "list"
-)
-
-type imdbSpider struct {
-	*crawl.Crawl
-}
-
-func (spider *imdbSpider) List(ctx context.Context, resp *crawl.Response) (err error) {
-	if err := spider.checkError(ctx, resp); err != nil {
-		return err
-	}
-
-	resp.Query().Find("table.chart td.titleColumn a").Each(func(_ int, link *goquery.Selection) {
-		href, _ := link.Attr("href")
-		spider.Crawl.Schedule(&crawl.Request{
-			URL:       href,
-			Source:    resp,
-			Callbacks: crawl.Callbacks(Entity),
-		})
-	})
-
-	return
-}
-
-func (spider *imdbSpider) Entity(ctx context.Context, resp *crawl.Response) (err error) {
-	if err := spider.checkError(ctx, resp); err != nil {
-		return err
-	}
-
-	title := crawl.Text(resp, "h1.header span[itemprop=name]:nth-of-type(1)")
-	year := crawl.Text(resp, "h1.header span a")
-	log.Printf("Response: status=%q title=%q year=%s", resp.GetStatus(), title, year)
-
-	return
-}
-
-func (spider *imdbSpider) checkError(ctx context.Context, resp *crawl.Response) (err error) {
-	doh := crawl.Text(resp, "h1")
-	if doh == "D'oh!" {
-		return fmt.Errorf("IMDB returned: %q", crawl.Text(resp, "body"))
-	}
-	return
 }
---
+++ examples/imdb/spider/main.go
@@ -0,0 +1,65 @@
+// Package spider implements imdb spider.
+package spider
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/crackcomm/crawl"
+	"golang.org/x/net/context"
+)
+
+// Entity - Movie entity.
+var Entity = "imdb_entity"
+
+// List - Movies list.
+var List = "imdb_list"
+
+// Register - Registers imdb spider.
+func Register(c *crawl.Crawl) {
+	spider := &imdbSpider{Crawl: c}
+	c.Handler(Entity, spider.Entity)
+	c.Handler(List, spider.List)
+}
+
+type imdbSpider struct {
+	*crawl.Crawl
+}
+
+func (spider *imdbSpider) List(ctx context.Context, resp *crawl.Response) (err error) {
+	if err := spider.checkError(ctx, resp); err != nil {
+		return err
+	}
+
+	resp.Query().Find("table.chart td.titleColumn a").Each(func(_ int, link *goquery.Selection) {
+		href, _ := link.Attr("href")
+		spider.Crawl.Schedule(&crawl.Request{
+			URL:       href,
+			Source:    resp,
+			Callbacks: crawl.Callbacks(Entity),
+		})
+	})
+
+	return
+}
+
+func (spider *imdbSpider) Entity(ctx context.Context, resp *crawl.Response) (err error) {
+	if err := spider.checkError(ctx, resp); err != nil {
+		return err
+	}
+
+	title := crawl.Text(resp, "h1.header span[itemprop=name]:nth-of-type(1)")
+	year := crawl.Text(resp, "h1.header span a")
+	log.Printf("Response: status=%q title=%q year=%s", resp.GetStatus(), title, year)
+
+	return
+}
+
+func (spider *imdbSpider) checkError(ctx context.Context, resp *crawl.Response) (err error) {
+	doh := crawl.Text(resp, "h1")
+	if doh == "D'oh!" {
+		return fmt.Errorf("IMDB returned: %q", crawl.Text(resp, "body"))
+	}
+	return
+}
