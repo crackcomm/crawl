20a96c0c36c214f9c4bc028d4099228947be0b58
crackcomm <crackcomm@gmail.com>
2015-12-27 09:30:06 +0100

    open: make it Handler compatible

--- open/open.go
+++ open/open.go
@@ -6,13 +6,15 @@ import (
 	"os"
 	"path/filepath"
 
+	"golang.org/x/net/context"
+
 	"github.com/crackcomm/crawl"
 	"github.com/satori/go.uuid"
 	"github.com/skratchdot/open-golang/open"
 )
 
 // Open - Opens crawl response in browser.
-func Open(resp *crawl.Response) error {
+func Open(ctx context.Context, resp *crawl.Response) error {
 	fname := filepath.Join(os.TempDir(), fmt.Sprintf("%s.html", uuid.NewV4().String()))
 	body, err := resp.ReadBody()
 	if err != nil {
